4b9b5e3b8397d4b0d51cc3b6977168bed7a1d600
Kay Schäfer <148460726+Neuroklast@users.noreply.github.com>
2026-03-10 16:34:27 +0000

    Generated by Spark: Fix all reported errors.

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,6 @@ import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 import { LocaleProvider } from './contexts/LocaleContext.tsx'
 
-import '@fontsource/jetbrains-mono/400.css'
-import '@fontsource/jetbrains-mono/500.css'
-import '@fontsource/jetbrains-mono/700.css'
-import '@fontsource/space-grotesk/400.css'
-import '@fontsource/space-grotesk/500.css'
-import '@fontsource/space-grotesk/600.css'
-
 import "./main.css"
 import "./styles/theme.css"
 import "./index.css"
